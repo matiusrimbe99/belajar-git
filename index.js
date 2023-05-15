82946e6084a2c7d7a2832a039b685c961624f491
matiusrimbe99 <matiusrimbe.ptik@gmail.com>
2023-05-16 00:42:11 +0800

    menambahkan fitur perkalian

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ const subtract = (a, b) => a - b;
 
 const resultSubtract = subtract(20, 4);
 
+const multiply = (a, b) => a * b;
+
+const resultMultiply = multiply(20, 3);
+
 console.log(resultAdd);
 console.log(resultSubtract);
+console.log(resultMultiply);
